32db0c1c871619a88dbfe5ef2b4e8ff62cb71f38
Leonardo Menezes Soares de Azevedo <leomenezes12345@gmail.com>
2022-10-22 13:29:01 -0300

    added some images and started working with rects

--- blank.py
+++ blank.py
@@ -4,17 +4,24 @@ import pygame
 
 pygame.init()
 screen = pygame.display.set_mode((800, 400))
-pygame.display.set_caption('Rage Square')
+pygame.display.set_caption('Game Name')
 clock = pygame.time.Clock()
 
 test_font = pygame.font.Font('fonts/SHPinscher-Regular.otf', 50)
 
-background = pygame.image.load('graphics/background1.png')
-first_character = pygame.image.load('graphics/characters/agent.png')
-text_font = test_font.render('Game Name', False, 'Yellow')
+background = pygame.image.load('graphics/background2.png').convert()
 
-char_xPos = 350
-char_yPos = 200
+tileGrass1 = pygame.image.load('graphics/platforms/grass1.png').convert()
+
+first_character = pygame.image.load(
+    'graphics/characters/agent.png').convert_alpha()
+first_char_rect = first_character.get_rect(topleft=(350, 300))
+
+main_character = pygame.image.load(
+    'graphics/characters/tile000.png').convert_alpha()
+main_rect = main_character.get_rect(topleft=(75, 300))
+
+text_font = test_font.render('Game Name', False, 'Yellow')
 
 while True:
     for event in pygame.event.get():
@@ -23,15 +30,19 @@ while True:
             exit()
 
     screen.blit(background, (0, 0))
+    screen.blit(tileGrass1, (50, 300))
     screen.blit(text_font, (300, 50))
 
-    if char_xPos > 500 and char_yPos < -100:
-        char_xPos = -50
-        char_yPos = 400
+    main_rect.left += 3
+    if main_rect.left > 800:
+        main_rect.right = 0
 
-    char_xPos += 2
-    char_yPos -= 1
-    screen.blit(first_character, (char_xPos, char_yPos))
+    mouse_pos = pygame.mouse.get_pos()
+    if main_rect.collidepoint(mouse_pos):
+        print('ó os cara se batendo sksksksk')
+
+    screen.blit(first_character, (first_char_rect))
+    screen.blit(main_character, (main_rect))
 
     pygame.display.update()
     clock.tick(60)
