2997d3d8641c30f0277ad1c661cd9a0c1c5612b1
Leonardo Menezes Soares de Azevedo <leomenezes12345@gmail.com>
2022-10-24 15:03:35 -0300

    Added an Enemy and collision

--- code.py
+++ code.py
@@ -6,6 +6,7 @@ pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Game Name')
 clock = pygame.time.Clock()
+game_active = True
 
 test_font = pygame.font.Font('fonts/SHPinscher-Regular.otf', 50)
 
@@ -15,15 +16,15 @@ background = pygame.image.load('graphics/background2.png').convert()
 tileGrass1 = pygame.image.load('graphics/platforms/grass1.png').convert()
 
 first_character = pygame.image.load(
-    'graphics/characters/agent.png').convert_alpha()
-first_char_rect = first_character.get_rect(topleft=(350, 300))
+    'graphics/characters/enemy_1.png').convert_alpha()
+first_char_rect = first_character.get_rect(topleft=(350, 314))
 
 main_character = pygame.image.load(
     'graphics/characters/tile000.png').convert_alpha()
 main_rect = main_character.get_rect(bottomleft=(75, 364))
 player_grav = 0
 
-main_title = test_font.render('Game Name', False, '#E9D5D8')
+main_title = test_font.render('Corre Walter!', False, '#E9D5D8')
 title_rect = main_title.get_rect(center=(400, 50))
 
 while True:
@@ -40,30 +41,40 @@ while True:
             if event.key == pygame.K_SPACE and main_rect.bottom == 365:
                 player_grav = -20
 
-    screen.blit(background, (0, 0))
-    screen.blit(tileGrass1, (-60, 336))
-    screen.blit(tileGrass1, (0, 336))
-    screen.blit(tileGrass1, (90, 336))
-    screen.blit(tileGrass1, (180, 336))
-    screen.blit(tileGrass1, (270, 336))
-    screen.blit(tileGrass1, (360, 336))
-    screen.blit(tileGrass1, (450, 336))
-    screen.blit(tileGrass1, (540, 336))
-    screen.blit(tileGrass1, (630, 336))
-    screen.blit(tileGrass1, (720, 336))
+    if game_active:
 
-    pygame.draw.rect(screen, '#52ABE6', title_rect)
-    pygame.draw.rect(screen, '#52ABE6', title_rect, 10)
-    screen.blit(main_title, (title_rect))
+        screen.blit(background, (0, 0))
+        screen.blit(tileGrass1, (-60, 336))
+        screen.blit(tileGrass1, (0, 336))
+        screen.blit(tileGrass1, (90, 336))
+        screen.blit(tileGrass1, (180, 336))
+        screen.blit(tileGrass1, (270, 336))
+        screen.blit(tileGrass1, (360, 336))
+        screen.blit(tileGrass1, (450, 336))
+        screen.blit(tileGrass1, (540, 336))
+        screen.blit(tileGrass1, (630, 336))
+        screen.blit(tileGrass1, (720, 336))
 
-    screen.blit(first_character, (first_char_rect))
+        pygame.draw.rect(screen, '#52ABE6', title_rect)
+        pygame.draw.rect(screen, '#52ABE6', title_rect, 10)
+        screen.blit(main_title, (title_rect))
 
-    # Player
-    player_grav += 1
-    main_rect.y += player_grav
-    if main_rect.bottom >= 365:
-        main_rect.bottom = 365
-    screen.blit(main_character, (main_rect))
+        first_char_rect.left -= 4
+        if first_char_rect.right <= 0:
+            first_char_rect.left = 800
+        screen.blit(first_character, (first_char_rect))
+
+        # Player
+        player_grav += 1
+        main_rect.y += player_grav
+        if main_rect.bottom >= 365:
+            main_rect.bottom = 365
+        screen.blit(main_character, (main_rect))
+
+        # Collision with Enemy
+        if first_char_rect.colliderect(main_rect):
+            pygame.quit()
+            exit()
 
     pygame.display.update()
     clock.tick(60)
